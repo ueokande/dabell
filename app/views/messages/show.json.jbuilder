99f75eba06a59708c879e7a6c504d9276ecd113a
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-04 22:54:36 +0900

    Add message scaffold

---
+++ app/views/messages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @message, :id, :text, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :messages
+  resources :messages
+  resources :messages
   devise_for :users
 end
