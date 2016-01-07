85fa300add01e90d50e83f6e0f5968fdc0adbdc6
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-07 22:58:10 +0900

    Merge HomeController to MessageController

--- spec/routing/messages_routing_spec.rb
+++ spec/routing/messages_routing_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 RSpec.describe MessagesController, type: :routing do
   describe "routing" do
 
+    it "routes to #index by root path" do
+      expect(:get => "/").to route_to("messages#index")
+    end
+
     it "routes to #index" do
       expect(:get => "/messages").to route_to("messages#index")
     end
