25d6764889b6b0511ed489a937cf16eabf3d8c2d
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-11 15:03:09 +0900

    Remove deprecation from out codes

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe "Messages", type: :request do
 
     describe "GET /messages" do
       it "returns success" do
-        get messages_path, {format: 'json'}
+        get messages_path,
+          params: { format: 'json' }
         expect(response).to have_http_status(200)
       end
     end
@@ -16,7 +17,8 @@ RSpec.describe "Messages", type: :request do
   context 'when user is not logged in' do
     describe "GET /messages" do
       it "returns Unauthorized" do
-        get messages_path, {format: 'json'}
+        get messages_path,
+          params: { format: 'json' }
         expect(response).to have_http_status(:unauthorized)
       end
     end
