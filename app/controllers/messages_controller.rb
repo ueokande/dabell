7bf95a655f977b29083f8c880bb6daf904d056ec
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-16 15:07:19 +0900

    CHange as broadcast into controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,6 +17,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       render :show, status: :created, location: @message
+      MessageBroadcastJob.perform_later(@message)
     else
       render json: @message.errors, status: :unprocessable_entity
     end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ActiveRecord::Base
   validates :text, :user, presence: true
   belongs_to :user
-  after_create_commit { MessageBroadcastJob.perform_later self }
 end
