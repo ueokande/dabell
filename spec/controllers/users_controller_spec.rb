776a71cd065251767076a1108103026f8805c936
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-30 14:14:48 +0900

    Fix broken test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,28 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
-  describe "GET index" do
+  before { sign_in FactoryGirl.create(:user) }
+
+  describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET new" do
+  describe "GET #new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET create" do
+  describe "POST #create" do
+    let(:attributes) { FactoryGirl.attributes_for(:user) }
+
     it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+      post :create, { user: attributes }
+      expect(response).to redirect_to(users_path)
     end
   end
 
